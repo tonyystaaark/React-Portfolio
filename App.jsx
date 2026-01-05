2b85d770ae38f312b9dd94cefb1265c33cedca6c
tonyystaaark <144714043+tonyystaaark@users.noreply.github.com>
2026-01-05 23:55:19 +0800

    First Attempt

---
+++ App.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import NavContact from "./components/NavContact";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      {/* <NavContact /> */}otendako
+    </>
+  );
+}
+
+export default App;
